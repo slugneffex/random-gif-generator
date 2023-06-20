7fbf4d5cf3c85038e94c92403550eeb5bba2125b
slugneffex <rawatmayank969@gmail.com>
2023-06-20 18:19:01 +0530

    code complete

---
+++ src/components/Spinner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+        <div>
+            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+    )
+}
+
+export default Spinner
